32790c32b63701db4dab9351a5fe56c3919f4db6
maba <iq1359@naver.com>
2025-05-22 17:28:33 +0900

    ✅ [React]5일차 과제 제출
    refactor: adjust logic and update input styling

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    { id: 0, content: "공부하기" },
+    { id: 1, content: "청소하기" },
+    { id: 2, content: "" },
+    { id: 3, content: "바보" },
+    { id: 4, content: "멍청이" },
+    { id: 5, content: "이재헌 === 이재현" },
   ]);
 
   return (
@@ -33,53 +36,86 @@ function TodoInput({ todoList, setTodoList }) {
           setTodoList(newTodoList);
           setInputValue("");
         }}
-      >
-        추가하기
-      </button>
+      >추가하기</button>
     </>
   );
 }
 
 function TodoList({ todoList, setTodoList }) {
   return (
-    <ul>
-      {todoList.map((todo) => (
-        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
-      ))}
-    </ul>
+    <>
+      <div className="todoList">
+        <ul>
+          {todoList.map((todo) => (
+            <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
+function EditingInput({ setEditingIs, todo, inputValue, setTodoList, setInputValue }) {
+
+  const handlediting = () => {
+    setTodoList((prev) =>
+      prev.map((el) =>
+        el.id === todo.id ? { ...el, content: inputValue } : el
+      )
+    );
+    setEditingIs(false);
+  }
+  return (
+    <>
+      <input className="todoInput" value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className="opbtn" onClick={() => setEditingIs(false)}>취소</button>
+      <button className="opbtn" onClick={handlediting}>변경</button>
+    </>
+  )
+}
+
+// setTodoList((prev) =>
+//   prev.map((el) =>
+//     el.id === todo.id ? { ...el, content: inputValue } : el
+//   )
+// );
+
+
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const [editingIs, setEditingIs] = useState(false);
   return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
-    </li>
+    <>
+      <li>
+        <input type="checkbox" />
+        {todo.content}
+      </li>
+      <div>
+
+        {!editingIs && (
+          <button className="opbtn" onClick={() => setEditingIs(true)}>수정</button>
+        )}
+
+        {editingIs && < EditingInput
+          todo={todo} editingIs={editingIs}
+          setEditingIs={setEditingIs}
+          setInputValue={setInputValue}
+          inputValue={inputValue}
+          setTodoList={setTodoList} />}
+
+        {!editingIs && (<button className="opbtn"
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}
+        >삭제</button>)}
+      </div>
+
+    </>
+
   );
 }
 
