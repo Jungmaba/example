5856079db5fb5624b3a17abffb0188d8f747f121
maba <iq1359@naver.com>
2025-05-30 15:46:52 +0900

    code error

--- oz-style-practice/src/App.jsx
+++ oz-style-practice/src/App.jsx
@@ -1,19 +1,39 @@
-import "./App.scss";
+import styled, { createGlobalStyle } from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { flexMixin } from "./styled/styled";
+
+const StyledMainSection = styled.section`
+    ${flexMixin({ justify: "center", align: "center", wrap: "wrap", gap: "20px" })};
+    padding: 20px 40px;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: white;
+}
+
+body {
+  background-color: #121212;
+}
+`;
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <GlobalStyle />
+            <Header />
+            <StyledMainSection>
+                {contents.map((el) => (
+                    <Content key={el.id} content={el} />
+                ))}
+            </StyledMainSection>
+        </main>
+    );
 }
 
 export default App;
--- oz-style-practice/src/components/Header.jsx
+++ oz-style-practice/src/components/Header.jsx
@@ -1,12 +1,31 @@
+import styled from "styled-components";
+import { flexMixin, fontMixin, flexMixin2 } from "../styled/styled";
+/* ${flexMixin({ justify: "space-between", align: "center" })}; */
+const StyledHeader = styled.header`
+    ${flexMixin2("row", "space-betwenn", "center")};
+    padding: 20px 20px;
+    background-color: black;
+    ul {
+        ${flexMixin2("row", "space-betwenn", "center", "stretch", "20px")};
+
+        /* ${flexMixin({ justify: "space-betwenn", align: "center", gap: "20px" })}; */
+
+        li {
+            ${fontMixin({ size: "16px", weight: "400" })};
+            list-style: none;
+        }
+    }
+`;
+
 export default function Header() {
-  return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
-  );
+    return (
+        <StyledHeader>
+            <h1>OZ코딩스쿨</h1>
+            <ul>
+                <li>로그인</li>
+                <li>회원가입</li>
+                <li>내클래스</li>
+            </ul>
+        </StyledHeader>
+    );
 }
